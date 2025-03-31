3fb7c64bab1bb81d1852631d9ffd34d8439d8584
riverat <riverat@msoe.edu>
2025-03-30 20:20:30 -0500

    Game Finished (Fixed many bugs)

--- cmake-build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_SPA4_FILE C:/Users/riverat/SPA4/cmake-build-debug/SPA4.exe)
-set(__QT_DEPLOY_TARGET_SPA4_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_SPA4_RUNTIME_DLLS C:/Qt/6.8.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Core.dll)
